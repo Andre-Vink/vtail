6c58cc5dea53d1032361d6c3db626a32d8db314d
André Vink <13725527+Andre-Vink@users.noreply.github.com>
2018-01-25 17:23:57 +0100

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,28 @@
+use std::path::Path;
+use std::fs;
+use std::env;
 
+fn main() {
+    let args: env::Args = env::args();
+    let args: Vec<String> = args.collect();
+
+    if args.len() < 2 {
+        panic!("USAGE: ptail <extension>. EXAMPLE: ptail txt");
+    }
+
+    let extension: &str = &args[1];
+
+    println!("PTailing files ending with {}...", extension);
+
+    let path: &Path = Path::new(".");
+    println!("Path = {:?}", path);
+
+    let cur_dir: fs::ReadDir = fs::read_dir(path).expect("Cannot read current directory");
+    println!("CurDir = {:?}", cur_dir);
+
+    for entry in cur_dir {
+        if let Ok(entry) = entry {
+            println!("Entry {:?}", entry.path());
+        }
+    }
+}
