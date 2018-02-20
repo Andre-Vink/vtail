f6849d17410a64d022bf5869b6440d66c9db43b8
André Vink <djevink@gmail.com>
2018-02-20 06:54:26 +0100

    Major upscaled error handling.

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use std::env;
 use std::path::Path;
 use std::fs;
 use std::fs::File;
+use std::io;
 use std::io::Read;
 use std::io::Seek;
 use std::io::SeekFrom;
@@ -23,29 +24,35 @@ use core::cmp::Ordering;
 fn main() {
     let mut file_map: HashMap<String, u64> = HashMap::new();
 
-    let path_to_watch: PathBuf = get_path_to_watch();
-    let path_to_watch: &Path = path_to_watch.as_path();
-    println!("Tailing files in directory [{:?}]...", path_to_watch);
+    match get_path_to_watch() {
+        Err(e) => eprintln!("Cannot access current directory. Error: [{}]", e),
+        Ok(path_to_watch) => {
+            let path_to_watch: &Path = path_to_watch.as_path();
+            println!("Tailing files in directory [{:?}]...", path_to_watch);
 
-    let rd = fs::read_dir(&path_to_watch);
-    match rd {
-        Ok(rd) => read_directory(&mut file_map, rd),
-        Err(err) => println!("Dir no good: {:?}", err),
+            match fs::read_dir(&path_to_watch) {
+                Ok(rd) => read_directory(&mut file_map, rd),
+                Err(err) => eprintln!("Dir no good: {:?}", err),
+            }
+
+            // start tailing
+            tail(&mut file_map, &path_to_watch);
+
+            println!("Tailing ended.");
+        }
     }
-
-    // start tailing
-    tail(&mut file_map, &path_to_watch);
-
-    println!("Tailing ended.");
 }
 
-fn get_path_to_watch() -> PathBuf {
+fn get_path_to_watch() -> io::Result<PathBuf> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        env::current_dir().unwrap()
+        env::current_dir()
     } else {
         let pb = PathBuf::from(&args[1]);
-        env::current_dir().unwrap().join(pb)
+        match env::current_dir() {
+            Ok(cd) => Ok(cd.join(pb)),
+            Err(x) => Err(x),
+        }
     }
 }
 
@@ -62,10 +69,8 @@ fn tail(file_map: &mut HashMap<String, u64>, path_to_watch: &Path) {
     watcher.watch(path_to_watch, RecursiveMode::Recursive).unwrap();
 
     loop {
-        match rx.recv() {
-            Ok(RawEvent{path: Some(file), op: Ok(WRITE), ..}) =>
-                echo_file(file_map, file.as_path(), path_to_watch),
-            _ => {},
+        if let Ok(RawEvent{path: Some(file), op: Ok(WRITE), ..}) = rx.recv() {
+            echo_file(file_map, file.as_path(), path_to_watch);
         }
     }
 }
@@ -75,7 +80,7 @@ fn read_directory(file_map: &mut HashMap<String, u64>, rd: fs::ReadDir) {
     for entry in rd {
         match entry {
             Ok(de) => process_entry(file_map, de),
-            Err(err) => println!("Entry no good: {}", err),
+            Err(e) => eprintln!("Cannot read directory. Error: [{}]", e),
         }
     }
 }
@@ -90,34 +95,41 @@ fn process_entry(file_map: &mut HashMap<String, u64>, de: fs::DirEntry) {
 }
 
 fn process_file(file_map: &mut HashMap<String, u64>, p: &Path) {
-    let name = String::from(p.to_str().expect("Cannot get file name from path."));
-    let meta = p.metadata().expect("Cannnot get file length from path.");
-    let length = meta.len();
-
-//    println!("File  = [{}]=[{}]", name, length);
-
-    file_map.insert(name, length);
+    match p.to_str() {
+        None => eprintln!("Cannot get file name from path [{:?}].", p),
+        Some(s) => {
+            let name = String::from(s);
+            match p.metadata() {
+                Err(e) => eprintln!("Cannnot get the length from path [{:?}]. Error: [{}].", p, e),
+                Ok(meta) => {
+                    let length = meta.len();
+//                    println!("File  = [{}]=[{}]", name, length);
+                    file_map.insert(name, length);
+                }
+            }
+        }
+    }
 }
 
 fn echo_file(file_map: &mut HashMap<String, u64>, file: &Path, path_to_watch: &Path) {
     let parent_path = file.parent().unwrap().to_path_buf();
 //    println!("path_to_watch=[{:?}], parent_path=[{:?}]", path_to_watch, parent_path);
-    match path_to_watch.cmp(&parent_path) {
-        Ordering::Equal => {
-            let name = String::from(file.to_str().expect("Cannot get file name from path."));
 
-//            println!("WRITE to {:?} name = {}", file, name);
-            match file_map.get(&name) {
-                Some(fp) => {
-                    echo_file_from(&name, *fp);
-                },
-                None => {
-                    echo_whole_file(&name);
-                },
+    // Don't do files in sub folders
+    if let Ordering::Equal = path_to_watch.cmp(&parent_path) {
+        match file.to_str() {
+            None => eprintln!("Cannot get file name from path [{:?}].", file),
+            Some(s) => {
+                let name = String::from(s);
+
+//                    println!("WRITE to {:?} name = {}", file, name);
+                match file_map.get(&name) {
+                    Some(fp) => echo_file_from(&name, *fp),
+                    None     => echo_whole_file(&name),
+                }
+                process_file(file_map, file);
             }
-            process_file(file_map, file);
-        },
-        _ => (),
+        }
     }
 }
 
@@ -131,14 +143,18 @@ fn echo_file_from(file_name: &String, fp: u64) {
     let file_result = File::open(file_name);
     match file_result {
         Ok(mut file) => {
-            file.seek(SeekFrom::Start(fp))
-                .expect(&format!("Could no seek in open file [{}].", file_name));
-            let mut content = String::new();
-            file.read_to_string(&mut content)
-                .expect(&format!("Could not read file [{}].", file_name));
-            print!("{}", content);
-
+            match file.seek(SeekFrom::Start(fp)) {
+                Err(e) => eprintln!("Could no seek in open file [{}]. Error: [{}]", file_name, e),
+                Ok(_) => {
+                    let mut content = String::new();
+                    match file.read_to_string(&mut content) {
+                        Err(e) => eprintln!("Could not read file [{}]. Error: [{}]", file_name, e),
+                        Ok(_) => print!("{}", content),
+                    }
+                }
+            }
         },
-        Err(x) => println!("PTAIL ERROR => Could not open file [{}] for reading: error = [{}].", file_name, x),
+        Err(x) => println!(
+            "PTAIL ERROR => Could not open file [{}] for reading. Error: [{}].", file_name, x),
     }
 }
